cc5bffc4377e5e44c224aca3a5fb134a41827874
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-07-08 12:21:37 -0500

    Bonus Mission 1 & 2 Completed

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ window.addEventListener("load", function () {
                     <h3>${json[i].firstName} ${json[i].lastName}</h3>
                         <ul>
                             <li>Hours in space: ${json[i].hoursInSpace}</li>
-                            <li>Active: ${json[i].active}</li>
+                            <li class="activeTrue">Active: ${json[i].active}</li>
                             <li>Skills: ${json[i].skills}</li>
                         </ul>
                 </div>
@@ -61,7 +61,7 @@ window.addEventListener("load", function () {
                 }
             };
         });
-                container.innerHTML =+ `<p style="color:red"> There are ${json.length} Astronauts up in here.</p>`  
+                container.innerHTML =+ `There are ${json.length} Astronauts up in here.</p>`  
     });
 
 });
